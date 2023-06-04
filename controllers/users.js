e9471eefa480b5df0e35268b07ecf94ea9109476
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-04 22:22:18 +0200

    feat: add updateUser updateAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -14,9 +14,29 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar})
+  User.create({ name, about, avatar })
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => res.status(500).send({ message: 'Error creating user' }));
 };
 
-module.exports = { getAllUsers, getUserById, createUser };
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Error update user' }));
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Error update user avatar' }));
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  updateAvatar,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
 
-const { getAllUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  updateAvatar,
+} = require('../controllers/users');
 
 router.get('/users', getAllUsers);
 router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
