3ccf380f5748be3a61a56b12d3644321dfd0094d
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-04 21:14:04 +0200

    feat: _id hardcoded

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '647cda86587d416c8c4e0187'
+  };
+  next();
+});
+
 app.use(usersRouter);
+// app.use(cardsRouter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
