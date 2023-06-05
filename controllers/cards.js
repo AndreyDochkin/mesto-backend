0bb97e236ac70f00d8859aef9c178b4492f1a6da
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-05 13:43:00 +0200

    feat: add Error handle card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,43 +1,83 @@
+const mongoose = require('mongoose');
 const Card = require('../models/card');
+const ValidationError = require('../errors/ValidationError');
+const UnhandledError = require('../errors/UnhandledError');
+const NotFoundError = require('../errors/NotFoundError');
 
-const getAllCards = (req, res) => {
+const getAllCards = (req, res, next) => {
   Card.find({})
     .then((allCards) => res.status(200).send({ data: allCards }))
-    .catch((err) => res.status(500).send({ message: 'Error getting all cards' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        throw new ValidationError(err.message);
+      }
+      throw new UnhandledError(err.message);
+    })
+    .catch((err) => next(err));
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner, createdAt: Date.now() })
     .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        throw new ValidationError(err.message);
+      }
+      throw new UnhandledError(err.message);
+    })
+    .catch((err) => next(err));
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error deleting card' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        throw new NotFoundError(err.message);
+      }
+      throw new UnhandledError(err.message);
+    })
+    .catch((err) => next(err));
 };
 
-const addLike = (req, res) => {
+const addLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
     .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        throw new ValidationError(err.message);
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        throw new NotFoundError(err.message);
+      }
+      throw new UnhandledError(err.message);
+    })
+    .catch((err) => next(err));
 };
 
-const deleteLike = (req, res) => {
+const deleteLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true }
   )
     .then((card) => res.status(200).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error delete like card' }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        throw new ValidationError(err.message);
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        throw new NotFoundError(err.message);
+      }
+      throw new UnhandledError(err.message);
+    })
+    .catch((err) => next(err));
 };
 
 module.exports = {
