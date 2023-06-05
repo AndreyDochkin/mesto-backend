a9a47fc12658112ca7ba7f4f3424249621cd4cd5
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-05 16:38:10 +0200

    hotfix: test errors 3

--- controllers/users.js
+++ controllers/users.js
@@ -48,7 +48,7 @@ const createUser = (req, res, next) => {
 
 const updateUser = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail((err) => {
       next(new NotFoundError('User not found'));
     })
@@ -65,7 +65,7 @@ const updateUser = (req, res, next) => {
 
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail((err) => {
       next(new NotFoundError('User not found'));
     })
