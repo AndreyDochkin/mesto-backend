c0107671f13f59ade487a9f16852bebc8620d2eb
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-05 13:43:30 +0200

    feat: add 404 page route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
@@ -18,6 +19,10 @@ app.use((req, res, next) => {
 
 app.use(usersRouter);
 app.use(cardsRouter);
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Page not found'));
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
