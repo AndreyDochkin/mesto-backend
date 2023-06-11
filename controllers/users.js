f0b6d6d526bd893f5ee2816e2714af1c3f70d6ed
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-11 19:30:20 +0200

    feat: add .env

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const BadRequest = require('../errors/BadRequest ');
 const UnhandledError = require('../errors/UnhandledError');
 const NotFoundError = require('../errors/NotFoundError');
 const Conflict = require('../errors/Conflict');
+const Unauthorized = require('../errors/Unauthorized');
 
 const MONGO_DUMPLICATE_KEY = 11000;
+
+const { JWT_SECRET } = process.env;
 const getAllUsers = (req, res, next) => {
   User.find({})
     .then((allUsers) => res.status(200).send({ data: allUsers }))
@@ -37,11 +42,17 @@ const getUserById = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
   bcrypt.hash(password, 10)
     .then((hash) => {
-      User.create({ name, about, avatar, email, password: hash })
-        .then((user) => res.status(200).send({ name, about, avatar, email }))
+      User.create({
+        name, about, avatar, email, password: hash,
+      })
+        .then((user) => res.status(200).send({
+          name, about, avatar, email,
+        }))
         .catch((err) => {
           if (err instanceof mongoose.Error.ValidationError) {
             throw new BadRequest('Переданны невалидные данные');
@@ -84,8 +95,28 @@ const updateUserAvatar = (req, res, next) => {
   updateUser(req, res, next, { avatar });
 };
 
-const loginUser = (req, res) => {
-  res.status(200).send({ message: 'ok' });
+const loginUser = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .orFail(() => {
+      next(new Unauthorized('Пользователь не найден'));
+    })
+    .then((user) => Promise.all([user, bcrypt.compare(password, user.password)]))
+    .then(([user, matched]) => {
+      if (!matched) {
+        throw new UnhandledError('Неверный пароль');
+      }
+      console.log(JWT_SECRET);
+      res.status(200).send({ user });
+      //TODO make token
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        throw new BadRequest('Переданны невалидные данные');
+      }
+      throw new UnhandledError('На сервере произошла ошибка');
+    })
+    .catch((err) => next(err));
 };
 
 module.exports = {
