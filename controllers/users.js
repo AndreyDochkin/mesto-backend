238c74fd3b1a51afdc7c1b3db03a754ddd5fa60a
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-12 12:10:15 +0200

    hotfix: login 3

--- controllers/users.js
+++ controllers/users.js
@@ -117,7 +117,7 @@ const loginUser = (req, res, next) => {
     .then((user) => Promise.all([user, bcrypt.compare(password, user.password)]))
     .then(([user, matched]) => {
       if (!matched) {
-        next(new UnhandledError('Неверный пароль'));
+        next(new Unauthorized('Неверный пароль'));
       }
       const token = signToken(user._id);
       res.status(200).send({ token });
