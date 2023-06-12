73ed27092613651f44049f448d47a57d12f8c7a3
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-12 19:49:27 +0200

    hotfix: test 2

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   console.error(err.statusCode);
   console.error(err.message);
   const { statusCode = 500, message = 'На сервере произошла ошибка' } = err;
   res.status(statusCode).send({ message });
+  next();
 };
 
 module.exports = errorHandler;
