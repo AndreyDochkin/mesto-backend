b574c94c256ffd2cd99719b87169c246461b469b
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-19 15:16:06 +0200

    feat: add logger

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_URI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -14,6 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(usersRouter);
 app.use(cardsRouter);
 
@@ -21,6 +24,7 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError('Page not found'));
 });
 
+app.use(errorLogger);
 app.use(errors()); // ? joi celebrate errors
 app.use(errorHandler); // ? middleware for errors
 
