4b8b46fc2f47efdbda178bc13632cea6c664b28f
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-26 20:08:07 +0200

    hotfix: add cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -15,9 +15,9 @@ const cors = (req, res, next) => {
 
   res.header('Access-Control-Allow-Origin', '*');
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
