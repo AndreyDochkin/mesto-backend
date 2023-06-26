bd4ec55ea099c4d86a768fdd630a1b66fc629513
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-26 15:03:48 +0200

    hotfix: cors *

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,13 +5,15 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
-module.exports = (req, res, next) => {
+const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   res.header('Access-Control-Allow-Credentials', true);
+
+  res.header('Access-Control-Allow-Origin', '*');
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -26,3 +28,5 @@ module.exports = (req, res, next) => {
 
   return next();
 };
+
+module.exports = cors;
