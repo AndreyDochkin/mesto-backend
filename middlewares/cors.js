de843766d65167b9cce63028a4fe102888959511
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-26 20:42:59 +0200

    hotfix: cors 2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,11 +13,11 @@ const cors = (req, res, next) => {
 
   res.header('Access-Control-Allow-Credentials', true);
 
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', '*');
 
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
