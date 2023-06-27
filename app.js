e8488efed9d6a3de17799584697e31c2b6b53eb7
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-27 17:17:49 +0200

    hotfix: cors 13

--- app.js
+++ app.js
@@ -30,16 +30,12 @@ const app = express();
 //   }),
 // );
 
+app.use(cors());
+
 app.use(requestLogger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://picventures.nomoreparties.sbs');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 app.use(usersRouter);
 app.use(cardsRouter);
