0c7ea9cebfabaa7a94bee8ab5ee61d663b9137df
Andrei Dochkin <andreydochkin@yandex.ru>
2023-06-28 16:22:23 +0200

    feat: crash test

--- app.js
+++ app.js
@@ -8,32 +8,35 @@ const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, MONGO_URI } = require('./config');
+const { PORT, MONGO_URI = 'mongodb://127.0.0.1:27017/mestodb' } = require('./config');
 
 const app = express();
 
-const allowedCors = [
-  'https://localhost:3000',
-  'http://localhost:3000',
-  'http://picventures.nomoreparties.sbs',
-  'https://picventures.nomoreparties.sbs',
-  'http://api.picventures.nomoreparties.sbs',
-  'https://api.picventures.nomoreparties.sbs',
-];
-
-app.use(
-  cors({
-    origin: allowedCors,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: [
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'http://picventures.nomoreparties.sbs',
+    'https://picventures.nomoreparties.sbs',
+    'http://api.picventures.nomoreparties.sbs',
+    'https://api.picventures.nomoreparties.sbs',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.use(requestLogger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//! CRASH TEST REMOVE!!
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(usersRouter);
 app.use(cardsRouter);
